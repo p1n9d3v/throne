55a18e3b76be02d6e67117971ea5db2174813f7d
KIM BEOMJOON <p1n9@KIMs-MacBook-Air.local>
2024-05-23 19:17:32 +0900

    feat(Home): Remove Throne and Gear logo

--- app/page.tsx
+++ app/page.tsx
@@ -14,53 +14,7 @@ export default function Home() {
                     muted
                     playsInline
                 />
-                <ThroneAndGearLogo />
             </div>
         </div>
-    );
-}
-
-function ThroneAndGearLogo() {
-    return (
-        <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className={styles.ThroneAndGearLogo}
-        >
-            <defs>
-                <linearGradient
-                    id="gradient"
-                    x1="0%"
-                    y1="0%"
-                    x2="100%"
-                    y2="100%"
-                >
-                    <stop
-                        offset="0%"
-                        style={{ stopColor: '#f5deb3', stopOpacity: 1 }}
-                    />
-                    <stop
-                        offset="100%"
-                        style={{ stopColor: '#b8860b', stopOpacity: 1 }}
-                    />
-                </linearGradient>
-            </defs>
-            <text
-                x="400"
-                y="120"
-                className={styles.ThroneAndGearLogo__MainText}
-            >
-                THRONE
-            </text>
-            <text x="400" y="200" className={styles.ThroneAndGearLogo__And}>
-                AND
-            </text>
-            <text
-                x="400"
-                y="280"
-                className={styles.ThroneAndGearLogo__MainText}
-            >
-                GEAR
-            </text>
-        </svg>
     );
 }
