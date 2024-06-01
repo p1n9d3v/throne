f19c2c8f7a46894290c3192b0aae58f814778990
KIM BEOMJOON <p1n9@KIMs-MacBook-Air.local>
2024-06-01 18:33:23 +0900

    refactor(usePagination): refactoring Pagination

---
+++ app/hooks/usePagination.tsx
@@ -0,0 +1,68 @@
+import { queryPagination } from '@/api/firestore';
+import { Updater, PaginationState } from '@tanstack/react-table';
+import React from 'react';
+import {useQuery} from '@tanstack/react-query'
+
+interface Props {
+    paths: string[];
+    orderByField: string;
+    orderType: 'asc' | 'desc';
+}
+
+function usePagination({ paths.orderByField,orderType }: Props) {
+    const [lastVisible, setLastVisible] = React.useState<any>(undefined);
+    const [firstVisible, setFirstVisible] = React.useState<any>(undefined);
+    const [type, setType] = React.useState<'first' | 'prev' | 'next' | 'last'>(
+        'first',
+    );
+    const [nextPagination, setNextPagination] = React.useState({
+        pageIndex: 0,
+        pageSize: 5,
+    });
+    const [pagination, setPagination] = React.useState({
+        pageIndex: 0,
+        pageSize: 5,
+    });
+
+
+
+    const query = useQuery({
+        queryKey: ['weapons', pagination],
+        queryFn: async () => {
+            const resp = await queryPagination({
+                type,
+                pageSize: pagination.pageSize,
+                lastVisible,
+                firstVisible,
+				paths,
+				orderByField,
+				orderType
+            });
+            setLastVisible(resp.lastVisible);
+            setFirstVisible(resp.firstVisible);
+            return resp;
+        },
+    });
+
+
+	const onChangePage = (pageState:Updater<PaginationState>) => {
+		setNextPagination(pageState)
+	}
+
+    React.useEffect(() => {
+        const diff = nextPagination.pageIndex - pagination.pageIndex;
+        if (diff === 1) setType('next');
+        else if (diff === -1) setType('prev');
+        else if (diff < -1) setType('first');
+        else if (diff > 1) setType('last');
+        setPagination(nextPagination);
+    }, [nextPagination]);
+
+	return {
+		pagination,
+		onChangePage,
+		query
+	}
+}
+
+export default  usePagination
